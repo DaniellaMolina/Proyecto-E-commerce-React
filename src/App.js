c244ce2e96be0b0aec2a73723ccd95be49b82f16
Daniella Molina <lic.daniellamolina@gmail.com>
2022-05-24 21:10:24 -0300

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Navbar from './componentes/Navbar.js';
+import Encabezado from './componentes/Encabezado.js';
+
+import './App.css';
+
+function App() {
+  return (
+    
+    
+    <div >
+      <header>
+        <Navbar/>
+      </header>
+      <main role="main">
+        <Encabezado/>
+      </main>
+    </div>
+    
+    
+  );
+}
+
+export default App;
+
